c8a79c001cf8682b2e149f4cc30c35e7825e197d
Antti Leppä <antti.leppa@foyt.fi>
2013-08-24 15:31:54 +0300

    ** Added PaytrailException

---
+++ src/main/java/fi/foyt/paytrail/PaytrailException.java
@@ -0,0 +1,15 @@
+package fi.foyt.paytrail;
+
+public class PaytrailException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public PaytrailException(String message) {
+		super(message);
+	}
+	
+	public PaytrailException(Exception cause) {
+		super(cause);
+	}
+	
+}
